d5a9d4b63125769885a7501884df5475d178694f
Gabriel José <gabriel.josemelo@hotmail.com>
2023-09-19 15:09:48 -0300

    feat: add login process

--- src/controllers/account/login.ts
+++ src/controllers/account/login.ts
@@ -1,7 +1,39 @@
-import { loginPage } from '../../ui/account/login-page.ts'
-import { htmlBase } from '../../ui/tools/html-fn.ts'
-import { Content } from "@/controllers/tools/index.ts";
+import { formData, text } from 'zod-form-data';
+import { html } from '../../ui/tools/html-fn.ts'
+import { Content, cookies } from "@/controllers/tools/index.ts";
+import { string } from "zod";
+import { sql } from "@/database/client.ts";
+import { TokenService } from "@/services/crypto/index.ts";
 
-export function login() {
-  return Content.html(htmlBase(loginPage()))
+const loginSchema = formData({
+  email: text(string().email()),
+  password: text()
+})
+
+export async function login(req: Request) {
+  const loginData = await req.formData()
+  const validationResult = loginSchema.safeParse(loginData)
+
+  if (!validationResult.success) {
+    return Content.html(html`
+      Error
+    `)
+  }
+
+  const { email } = validationResult.data
+
+  type tmp = { id: string }
+
+  const [account] = await sql<tmp>/*sql*/`
+    select * from accounts
+    where email = ${email};
+  `
+  const token = await TokenService.create({ id: account.id })
+
+  return Content.noContent({
+    headers: {
+      'set-cookie': cookies({ token }),
+      'hx-redirect': '/home'
+    }
+  })
 }
--- src/database/migrations/01_create_accounts.sql
+++ src/database/migrations/01_create_accounts.sql
@@ -1,6 +1,6 @@
 create table if not exists accounts (
   id varchar(255) primary key not null,
   username varchar(255) not null,
-  email varchar(255) not null,
+  email varchar(255) unique not null,
   password varchar(255) not null
 );
